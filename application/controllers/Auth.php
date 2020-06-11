14c3252300406cd912c02f583a5c3a5664fd3305
Muhammadiqbal15 <m44iqbal@gmail.com>
2020-06-11 14:53:16 +0700

    Membuat Fitur User Activation

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -90,10 +90,11 @@ class Auth extends CI_Controller
             $this->load->view('Auth/registrasi', $data);
             $this->load->view('TemplateAuth/FooterAuth');
         } else {
+            $email = $this->input->post('email', true);
 
             $data = [
                 'nama' => htmlspecialchars($this->input->post('nama', true)),
-                'email' => htmlspecialchars($this->input->post('email', true)),
+                'email' => htmlspecialchars($email),
                 'foto' => 'default.png',
                 'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
                 'notelp' => htmlspecialchars($this->input->post('nomortlp', true)),
@@ -103,12 +104,112 @@ class Auth extends CI_Controller
                 'kelurahan' => htmlspecialchars($this->input->post('Kelurahan', true)),
                 'alamat' => htmlspecialchars($this->input->post('alamat', true)),
                 'role_id' => 2,
-                'is_active' => 1,
+                'is_active' => 0,
+                'date_created' => time()
+            ];
+
+            //siapkan token
+            $token = base64_encode( random_bytes(32));
+            $user_token = [
+                'email' => $email,
+                'token' => $token,
                 'date_created' => time()
             ];
 
             $this->db->insert('user', $data);
-            $this->session->set_flashdata('message', 'Berhasil Terdaftar');
+            $this->db->insert('user_token', $user_token);
+
+            $this->_sendemail($token, 'verify');
+            $this->session->set_flashdata('message', 'Berhasil Terdaftar! Tolong Aktifasi Akun Email Mu');
+            redirect('Auth/index');
+        }
+    }
+
+    private function _sendemail($token, $type)
+    {
+        $config = [
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'shopsmartetoko15@gmail.com',
+            'smtp_pass' => '$12345678$',
+            'smtp_port' => 465,
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8', 
+            'newline'   => "\r\n"
+        ];
+
+        $this->load->library('email',$config);
+        $this->email->initialize($config);
+
+        $this->email->from('shopsmartetoko15@gmail.com','AdminShopsmart'); 
+        $this->email->to($this->input->post('email', true));
+
+        if($type == 'verify'){
+            $this->email->subject('Akun Verifikasi');
+            $this->email->message('Klik link ini untuk verifikasi akunmu : <a href="'.base_url().'auth/verify?email=' . $this->input->post('email') . '&token=' . $token .'">Aktifasi</a>');
+        }
+        
+
+        if($this->email->send()){
+            return true;
+        }else{
+            echo $this->email->print_debugger();
+            die;
+        }
+    }
+
+
+    public function verify()
+    {
+        $email = $this->input->get('email');
+        $token = $this->input->get('token');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+
+        if($user){
+
+            $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+            if($user_token)
+            {
+
+                if(time()- $user_token['date_created'] < (60*60*24))
+                {
+                    $this->db->set('is_active', 1); 
+                    $this->db->where('email',$email);
+                    $this->db->update('user');
+
+                    $this->db->delete('user_token',['email' => $email]);
+
+                    $this->session->set_flashdata('eroremail','<div class="alert alert-success" role="alert">
+                    '.$email.' sudah aktif! Tolong Masuk Ke Akun Mu
+                    </div>');
+                    redirect('Auth/index');
+                }else{
+
+                    $this->db->delete('user',['email' => $email]);
+                    $this->db->delete('user_token', ['email' => $email]);
+
+                    $this->session->set_flashdata('eroremail','<div class="alert alert-danger" role="alert">
+                    Aktifasi Akun Gagal! Token Expired/Kadaluarsa.
+                    </div>');
+                    redirect('Auth/index');
+                }
+
+            }else{
+
+                $this->session->set_flashdata('eroremail','<div class="alert alert-danger" role="alert">
+                Aktifasi Akun Gagal! Token Salah.
+                </div>');
+                redirect('Auth/index');
+            }
+
+        }else{
+
+            $this->session->set_flashdata('eroremail','<div class="alert alert-danger" role="alert">
+            Aktifasi Akun Gagal! Email Salah.
+            </div>');
             redirect('Auth/index');
         }
     }
